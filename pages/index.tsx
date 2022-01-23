5c196e833d0feec2d37bb992f23b096c7741a5c9
SilenNaihin <silen.naihin@gmail.com>
2022-01-23 15:40:37 -0500

    wireframe index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,33 @@
-import type { NextPage } from 'next'
-import styled from 'styled-components'
+import type { NextPage } from "next";
+import styled from "styled-components";
 
 const Home: NextPage = () => {
   return (
     <GlobalContainer>
-      
+      <TopBar>
+        <AboutButton>ABOUT</AboutButton>
+        <OwnershipButton>NFT OWNERSHIP</OwnershipButton>
+        <VerifiedButton>VERIFIED LIST</VerifiedButton>
+        <LogIn>LOG IN</LogIn>
+      </TopBar>
+      <Logo />
+      <SearchTerms type="text" placeholder="Search the Metaverse..." />
+      <TermsContainer>
+        <TrendingText>Trending</TrendingText>
+        {terms.map((term, index) => (
+          <Term key={index}>
+            <ImageContainer>
+              <TermImage src={term.image}></TermImage>
+              <TextContainer>
+                <Title>{term.title}</Title>
+                <Description>{term.description}</Description>
+              </TextContainer>
+            </ImageContainer>
+          </Term>
+        ))}
+      </TermsContainer>
     </GlobalContainer>
   );
-}
+};
 
-export default Home
+export default Home;
